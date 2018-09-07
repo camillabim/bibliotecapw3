af1d0abb9b3573f5fa575a6ae79fa3a9fa232148
Camillabim <camillabim@hotmail.com.br>
2018-09-06 21:55:35 -0300

    Iniciando biblioteca

---
+++ App/HelloWorld.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Camilla
+ * Date: 06/09/2018
+ * Time: 21:52
+ */
+
+namespace App;
+
+
+class HelloWorld{
+
+    public function getHello(){
+        return "Olá mundo!";
+    }
+
+}
